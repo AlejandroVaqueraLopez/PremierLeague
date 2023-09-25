9496bcd26a206e69b7424b998877e2c6e2a9466d
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2023-09-24 23:35:20 -0700

    feat: adding all

---
+++ PremiereLeague/PremiereLeague/Model/City.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class City {
+    private int _id;
+    private string _name;
+    private bool _status;
+
+    public int Id { get { return _id; } set { _id = value; } }
+    public string Name { get { return _name; } set { _name = value; } }
+    public bool Status { get { return _status; } set { _status = value; } }
+
+    //constructors
+    public City()
+    {
+        _id = 0;
+        _name = "";
+        _status = true;
+    }
+    public City(int id, string name, bool status)
+    {
+        _id = id;
+        _name = name;
+        _status = status;
+    }
+}
---
+++ PremiereLeague/PremiereLeague/Model/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Player
+{
+    private int _id;
+    private string _name;
+    private string _lastName;
+    private DateTime _DOB;
+    private Team _teamId;
+    private int _number;
+    private string _position;
+    private bool _status;
+
+    public int Id { get { return _id; } set { _id = value; } }
+    public string Name { get { return _name; } set { _name = value; } }
+    public string LastName { get { return _lastName; } set { _lastName = value; } }
+    public DateTime DOB { get { return _DOB; } set { _DOB = value; } }
+    public Team TeamId { get { return _teamId; } set { _teamId = value; } }
+    public int Number { get { return _number; } set { _number = value; } }
+    public string Position { get { return _position; } set { _position = value; } }
+    public bool Status { get { return _status; } set { _status = value; } }
+
+    //constructors
+    public Player()
+    {
+        _id = 0;
+        _name = "";
+        _lastName = "";
+        _DOB = new DateTime();
+        _teamId = new Team();
+        _number = 0;
+        _position = "";
+        _status = true;
+    }
+    public Player(int id, string name, string lastName, DateTime dob, Team teamId, int number, string position, bool status)
+    {
+        _id = id;
+        _name = name;
+        _lastName = lastName;
+        _DOB = dob;
+        _teamId = teamId;
+        _number = number;
+        _position = position;
+        _status = status;
+    }
+}
+
